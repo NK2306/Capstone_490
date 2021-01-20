34c704be61866f2c7d4fe4a3eefffb99053d9a90
kh_guy <kh_guy@perf-imglab05.concordia.ca>
2021-01-19 21:55:25 -0500

    All graph generated

--- Dataset_A_HR.m
+++ Dataset_A_HR.m
@@ -32,8 +32,8 @@ addpath(genpath(pwd));
 load -ascii 'cmap_asymmetry.mat';
 
 %%%%%%%%%%%%%%%%% initial parameter %%%%%%%%%%%%%%%%%
-%subdir_list = {'VP001-NIRS','VP002-NIRS','VP003-NIRS','VP004-NIRS','VP005-NIRS','VP006-NIRS','VP007-NIRS','VP008-NIRS','VP009-NIRS','VP010-NIRS','VP011-NIRS','VP012-NIRS','VP013-NIRS','VP014-NIRS','VP015-NIRS','VP016-NIRS','VP017-NIRS','VP018-NIRS','VP019-NIRS','VP020-NIRS','VP021-NIRS','VP022-NIRS','VP023-NIRS','VP024-NIRS','VP025-NIRS','VP026-NIRS'};
-subdir_list = {'VP001-NIRS','VP002-NIRS','VP003-NIRS'};
+subdir_list = {'VP001-NIRS','VP002-NIRS','VP003-NIRS','VP004-NIRS','VP005-NIRS','VP006-NIRS','VP007-NIRS','VP008-NIRS','VP009-NIRS','VP010-NIRS','VP011-NIRS','VP012-NIRS','VP013-NIRS','VP014-NIRS','VP015-NIRS','VP016-NIRS','VP017-NIRS','VP018-NIRS','VP019-NIRS','VP020-NIRS','VP021-NIRS','VP022-NIRS','VP023-NIRS','VP024-NIRS','VP025-NIRS','VP026-NIRS'};
+%subdir_list = {'VP001-NIRS','VP002-NIRS','VP003-NIRS'};
 %subdir_list = {'VP001-NIRS'};
 ival_epo  = [-5 60]*1000; % epoch range (unit: msec)
 ival_base = [-5 -2]*1000; % baseline correction range (unit: msec)
@@ -76,7 +76,7 @@ for vp = 1 : length(subdir_list)
         epo_all.deoxy = proc_appendEpochs(epo_all.deoxy, epo.deoxy);
         epo_all.oxy   = proc_appendEpochs(epo_all.oxy,   epo.oxy);
     end
-	
+	%{
     %% Plot figures
 	%% Baseline correction
 	epo.deoxy = proc_baseline(epo.deoxy, ival_base);
@@ -96,14 +96,23 @@ for vp = 1 : length(subdir_list)
 	epo.deoxy = proc_average(epo.deoxy, 'Stats', 1);
 	epo.oxy   = proc_average(epo.oxy, 'Stats', 1);
 	
-	subplot(27,4,(vp*4 + 1));
-	plot_channel(epo.deoxy, 'AF7');
-	subplot(27,4,(vp*4 + 2));
-	plot_channel(epo.deoxy, 'C3h');
-	subplot(27,4,(vp*4 + 3));
-	plot_channel(epo.oxy, 'AF7');
-	subplot(27,4,(vp*4 + 4));
-	plot_channel(epo.oxy, 'C3h');
+    %% Plot figures
+	subplot(2,2,1);
+	plot_channel(epo.deoxy, 'AF7', 'YLim',[-0.006, 0.006]);
+	subplot(2,2,2);
+	plot_channel(epo.deoxy, 'C3h', 'YLim',[-0.006, 0.006]);
+	subplot(2,2,3);
+	plot_channel(epo.oxy, 'AF7', 'YLim',[-0.006, 0.006]);
+	subplot(2,2,4);
+	plot_channel(epo.oxy, 'C3h', 'YLim',[-0.006, 0.006]);
+    
+    %% Save Figure
+    cd('Figures');
+    num = string(vp);
+    name = strcat('Participant', num,'.jpg');
+    name2 = char(name);
+    saveas(gcf,name2);
+    %}
 	
 end
 
@@ -126,12 +135,17 @@ epo_all.deoxy = proc_average(epo_all.deoxy, 'Stats', 1);
 epo_all.oxy   = proc_average(epo_all.oxy, 'Stats', 1);
 
 %% Plot figures
-subplot(27,4,1);
-plot_channel(epo_all.deoxy, 'AF7');
-subplot(27,4,2);
-plot_channel(epo_all.deoxy, 'C3h');
-subplot(27,4,3);
-plot_channel(epo_all.oxy, 'AF7');
-subplot(27,4,4);
-plot_channel(epo_all.oxy, 'C3h');
+subplot(2,2,1);
+plot_channel(epo_all.deoxy, 'AF7', 'YLim',[-0.006, 0.006]);
+subplot(2,2,2);
+plot_channel(epo_all.deoxy, 'C3h', 'YLim',[-0.006, 0.006]);
+subplot(2,2,3);
+plot_channel(epo_all.oxy, 'AF7', 'YLim',[-0.006, 0.006]);
+subplot(2,2,4);
+plot_channel(epo_all.oxy, 'C3h', 'YLim',[-0.006, 0.006]);
+
+%% Save Figure
+cd(WorkingDir);
+cd('Figures');
+saveas(gcf,'OverAll.jpg');
 
